5c4ab3848c5243f31e077ccd2f945475db5bc843
vantuan25072001 <vantuan25072001@gmail.com>
2023-11-24 16:11:00 +0700

    fix authencation

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  console.log('alooooo');
 });
 
 app.use(globalErrorHandler);
