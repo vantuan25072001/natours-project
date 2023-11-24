34369249e35c023c96fd1e8304860b28b2ef105d
vantuan25072001 <vantuan25072001@gmail.com>
2023-11-24 17:05:58 +0700

    email

--- app.js
+++ app.js
@@ -24,10 +24,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
-<<<<<<< HEAD
   console.log('hiii');
-=======
->>>>>>> alooooo
+  console.log('email change');
   next();
 });
 
