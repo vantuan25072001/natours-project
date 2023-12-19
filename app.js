993a1672ba4409d44f9d56861b072093f5460eb4
vantuan25072001 <vantuan25072001@gmail.com>
2023-12-19 14:49:00 +0700

    master

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.use(express.static(`${__dirname}/public`));
 //Test middleware
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
+  console.log('master');
   next();
 });
 
