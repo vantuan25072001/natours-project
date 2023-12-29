7445534a89f3ea8d72d64b53799248183ac30a64
vantuan25072001 <vantuan25072001@gmail.com>
2023-12-29 11:45:26 +0700

    add frontend

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -70,13 +71,7 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Nguyen Van Tuan',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter); //passwordChangeAt
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
